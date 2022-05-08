ad6a527667e325a1f3aac8622db0cb23dd271135
Aadickk <adik54112@gmail.com>
2022-05-08 14:27:26 +0600

    Added order, delete and contains methods

--- src/MyBST.java
+++ src/MyBST.java
@@ -1,4 +1,6 @@
 public class MyBST {
+    Node node;
+
     class Node {
         int value;
         Node left;
@@ -12,7 +14,7 @@ public class MyBST {
     }
 
     public class MyBst {
-        Node node;
+
 
         public void add(int value) {
             node = addRec(node, value);
@@ -31,6 +33,48 @@ public class MyBST {
                 return node;
             }
             return node;
+        }
+    }
+
+    private boolean contNode(Node node, int value) {
+        if (node == null) {
+            return false;
+        }
+        if (value == node.value) {
+            return true;
+        }
+        return value < node.value;
+
+    }
+
+    public boolean contains(int value) {
+        return contNode(node, value);
+    }
+
+    private Node delete(Node node, int value) {
+        if (node == null) {
+            return null;
+        }
+        if (value < node.value) {
+            node.left = delete(node.left, value);
+            return node;
+            }
+        if (node.left == null) {
+            return node.right;
+        }
+        if (node.right == null) {
+            return node.left;
+        }
+        if (node.right == null && node.left == null) {
+            return null;
+        }
+        return node;
+    }
+    public void order(Node node){
+        if (node != null) {
+            order(node.left);
+            System.out.print(" " + node.value);
+            order(node.right);
         }
     }
 }
